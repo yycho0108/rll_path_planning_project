f22845324209c1b0623876bfe925b5d73563ead8
Jamie Cho <jchocholate@gmail.com>
2018-08-28 05:37:53 +0900

    completed (raw) graph construction

--- scripts/seg_proc.py
+++ scripts/seg_proc.py
@@ -11,6 +11,21 @@ def dist_p2l(p, l):
     d = np.abs(np.cross(p2-p1, p1-p))/np.linalg.norm(p2-p1)
     return d
 
+def dist_p2s(p, s):
+    p      = np.asarray(p, dtype=np.float32)
+    p1, p2 = np.asarray(s, dtype=np.float32)
+
+    sd = np.linalg.norm(p2 - p1)
+    if np.isclose(sd, 0):
+        return np.linalg.norm(p - p1)
+    t = np.dot(p - p1, p2 - p1) / (sd*sd)
+    if t < 0:
+        return np.linalg.norm(p - p1)
+    elif t > 1:
+        return np.linalg.norm(p - p2)
+    proj = p1 + t * (p2 - p1)
+    return np.linalg.norm(p - proj)
+
 class Graph(object):
     def __init__(self, nodes={}, edges={}):
         self._nodes = nodes # == [(x,y)]
@@ -30,11 +45,13 @@ class Graph(object):
                 return i
         return None
 
-    def match_edge(self, pt):
+    def match_edge(self, pt, tol=2e-2):
         # warning : match_edge WILL modify edges to accomodate nodes.
-        for (ep0,ep1) in self._edges:
-            pass
-        # TODO : impl.
+        for ei, (ep0,ep1) in enumerate(self._edges):
+            dist = dist_p2s(pt, [ep0,ep1])
+            if np.abs(dist) < tol:
+                return ei
+        return None
 
     def add_node(self, pt):
         # searches matching nodes if possible,
@@ -81,7 +98,7 @@ class SegProc(object):
             #self._yseg = self._yseg[np.logical_and(0.5<self._yseg[:,0,0], self._yseg[:,0,0]<1.0)]
             self._yseg = self.merge_y(self._yseg, dtol=7e-2, gtol=5e-2)#yseg #selg.prune(yseg)
 
-        self._joints = self.join_xy(self._xseg, self._yseg)
+        self._xseg, self._yseg, self._joints, self._con, self._dist = self.join_xy(self._xseg, self._yseg)
 
         self._seg = np.concatenate([self._xseg, self._yseg],axis=0)
         self._seg_i = 0
@@ -95,7 +112,7 @@ class SegProc(object):
         # create map for visualization
         n = np.ceil(w/r).astype(np.int32)
         m = np.ceil(h/r).astype(np.int32)
-        self._map = np.zeros(shape=(n,m), dtype=np.float32)
+        self._map = np.zeros(shape=(n,m,3), dtype=np.float32)
 
     def xy2uv(self, ps):
         # convert from physical coordinates to map coordinates
@@ -184,13 +201,25 @@ class SegProc(object):
 
     def join_xy(self, xseg, yseg, dtol=5e-2):
 
-        raw_joints = []
+        joints = []
 
         # join x-y segments
-        for xi, xx in enumerate(xseg):
-            for yi, yy in enumerate(yseg):
-                px0, px1 = xx
-                py0, py1 = yy
+        xseg = list(np.copy(xseg))
+        yseg = list(np.copy(yseg))
+        xi,yi = 0,0
+
+        xn0 = len(xseg)
+        yn0 = len(yseg)
+
+        while xi < len(xseg):
+            px0, px1 = xseg[xi]
+            yi = 0
+            slices = []
+            new_yseg = []
+
+            while yi < len(yseg):
+                py0, py1 = yseg[yi]
+
                 x, y = [py0[0], px0[1]] # point of intersection
 
                 in_x = (px0[0] < x < px1[0])
@@ -201,28 +230,60 @@ class SegProc(object):
                 x_good = (in_x or (dx < dtol))
                 y_good = (in_y or (dy < dtol))
 
-                #print dx, dy
-
                 if x_good and y_good:
-                    raw_joints.append([x,y,xi,yi])
+                    if xi<xn0:
+                        joints.append([x,y])
 
-        # process additional points
-        #nodes = [{'x':e[0], 'y':e[1], 'c':[]} for e in enumerate(raw_joints)]
+                    if in_y and dy > dtol:
+                        # split + append
+                        yseg[yi] =  [[x, py0[1]], [x, y]] # lesser half
+                        new_yseg.append([[x, y], [x, py1[1]]]) # greater half
 
-        #j_n = len(raw_joints)
-        #for i0 in range(j_n):
-        #    j0 = raw_joints[i0]
-        #    for i1 in range(i0+1, j_n):
-        #        j1 = raw_joints[i1]
-        #        if j0[2] == j1[2] or j0[3] == j1[3]: # on same segment
+                    if in_x and dx > dtol:
+                        slices.append(x)
+                yi += 1
+            yseg.extend(new_yseg)
 
+            if len(slices) > 0:
+                slices.append(px0[0])
+                slices.append(px1[0])
+                slices = sorted(slices)
 
-        #for j0 in raw_joints:
-        #    for j1 in raw_joints:
+                y = px0[1]
+                xseg[xi] = [[slices[0],y], [slices[1],y]]
+                sn = len(slices)
+                for i in range(1, sn-1):
+                    xseg.append([[slices[i],y], [slices[i+1],y]])
+
+                #for (x0, x1) in zip(slices[:-1], slices[1:]):
+                #    # split + append
+                #    xseg[xi] =  [[x0, y], [x, y]] # lesser half
+                #    xseg.append([[x, y], [x1, y]]) # greater half
+
+            xi += 1
+
+        #for xi, xx in enumerate(xseg):
+        #    for yi, yy in enumerate(yseg):
+        #        px0, px1 = xx
+        #        py0, py1 = yy
+        #        x, y = [py0[0], px0[1]] # point of intersection
+
+        #        in_x = (px0[0] < x < px1[0])
+        #        in_y = (py0[1] < y < py1[1])
+        #        dx = np.min(np.abs( [px0[0]-x, x-px1[0]] ))
+        #        dy = np.min(np.abs( [py0[1]-y, y-py1[1]] ))
+
+        #        x_good = (in_x or (dx < dtol))
+        #        y_good = (in_y or (dy < dtol))
+
+        #        #print dx, dy
+
+        #        if x_good and y_good:
+        #            joints.append([x,y])
+
+        joints = np.asarray(joints, dtype=np.float32) # == (N_J, 2)
 
         # add segment endpoints
-        joints = np.asarray(raw_joints)[:, :2]
-        joints = np.asarray(joints, dtype=np.float32) # == (N_J, 2)
         d_xs = np.reshape(joints, [-1,1,2]) - np.reshape(xseg, [1,-1,2]) # == (N_J, 2*N_X, 2)
         d_xs = np.linalg.norm(d_xs, axis=-1) # == (N_J, 2*N_X)
         xsel  = np.all(d_xs > dtol, axis=0)
@@ -234,94 +295,74 @@ class SegProc(object):
         j_y  = np.reshape(yseg, [-1,2])[ysel]
 
         joints = np.concatenate([joints, j_x, j_y], axis=0)
-        return joints
+        n_j = len(joints)
+        print 'Number of Joints : ', len(joints)
 
-        # kind of inefficient (but probably doesn't matter)
-        # new_xseg = []
-        # new_yseg = []
-        # for j in joints:
-        #     x, y = j
-        #     for xx in xseg:
-        #         px0, px1 = xx
+        # construct joint connectivity assignments from segments
+        con  = np.zeros(shape=(n_j,n_j), dtype=np.bool) # connectivity matrix
+        dist = np.full(shape=(n_j,n_j), fill_value=np.inf, dtype=np.float32) # edge distance matrix
 
-        #         in_x = (px0[0] < x < px1[0])
-        #         dx = np.min(np.abs( [px0[0]-x, x-px1[0]] ))
-        #         x_good = (in_x or (dx < dtol))
+        seg = np.concatenate([xseg, yseg], axis=0)
 
-        #         if in_x:
-        #             new_xseg.append([[px0[0],y], [x,y]])
-        #             new_xseg.append([[x,y], [px1[0],y]])
-        #         else:
-        #             new_xseg.append([[px0[0],y], [px1[0],y]])
+        print 'seg', len(seg)
+        for p0, p1 in seg:
+            print p0, p1
+            src = np.argmin(np.linalg.norm(joints - np.reshape(p0, [1,2]), axis=-1))
+            dst = np.argmin(np.linalg.norm(joints - np.reshape(p1, [1,2]), axis=-1))
+            con[src,dst] = con[dst,src] = True
+            dist[src,dst] = dist[dst,src] = np.linalg.norm((p0 - p1), axis=-1)
 
-        #     for yy in yseg:
-        #         py0, py1 = yy
+        # visualize matrix
+        #cv2.namedWindow('con', cv2.WINDOW_NORMAL)
+        #cv2.imshow('con', con.astype(np.float32))
 
-        #         in_y = (py0[1] < y < py1[1])
-        #         dy = np.min(np.abs( [py0[1]-y, y-py1[1]] ))
-        #         y_good = (in_y or (dy < dtol))
-
-        #         if in_y:
-        #             new_yseg.append([[x,py0[1]], [x,y]])
-        #             new_yseg.append([[x,y], [x,py1[1]]]) 
-        #         else:
-        #             new_yseg.append([[x,py0[1]], [x,py1[1]]])
-
-        # new_xseg = np.asarray(new_xseg, dtype=np.float32)
-        # new_yseg = np.asarray(new_yseg, dtype=np.float32)
-        # return joints, new_xseg, new_yseg
-
-    #def merge_hv(self, seg, tol=1e-2):
-    #    # merge horizontal or vertical segments
-    #    i = 0
-    #    n = len(seg)
-    #    new_seg = []
-    #    merged = []
-
-    #    for i in range(n):
-    #        if i in merged:
-    #            continue
-    #        for j in range(i+1, n):
-    #            if j in merged:
-    #                continue
-    #            s0a, s0b = seg[i] # 2 points
-    #            s1a, s1b = seg[j] # 2 points
-    #            d0a1a = np.linalg.norm(s0a-s1a)
-    #            d0a1b = np.linalg.norm(s0a-s1b)
-    #            d0b1a = np.linalg.norm(s0b-s1a)
-    #            d0b1b = np.linalg.norm(s0b-s1b)
-    #            dist_p2l(s0a
-    #            d = np.min([d0a1a, d0a1b, d0b1a, d0b1b])
-    #            if d <= tol:
-    #                merged.append(j)
-
-    #    return new_seg
+        return xseg, yseg, joints, con, dist
 
     def show(self):
         print self._seg_n
 
         cv2.namedWindow('segments', cv2.WINDOW_NORMAL)
-        while self._seg_i < self._seg_n:
-            #self._map.fill(0.)
-            p0, p1 = self._seg[self._seg_i] # == (2,2)
-            self._seg_i += 1
-            #if np.allclose(p0, p1):
-            #    continue
-            p0 = tuple(self.xy2uv(p0))
-            p1 = tuple(self.xy2uv(p1))
-            cv2.line(self._map, p0, p1, color=1.0, thickness=1)
 
-            cv2.imshow('segments', self._map)
-            cv2.waitKey(50)
-            #k = cv2.waitKey(0)
-            #if k == 27:
-            #    break
-
+        # draw joints
         for j in self._joints:
             pj = tuple(self.xy2uv(j))
-            cv2.circle(self._map, pj, radius=10, color=1.0)
+            cv2.circle(self._map, pj, radius=10, color=(1.0,1.0,1.0), thickness=1)
+            #cv2.circle(self._map, pj, radius=10, color=np.random.uniform(size=3), thickness=1)
+
             cv2.imshow('segments', self._map)
-            cv2.waitKey(20)
+            k = cv2.waitKey(10)
+            if k == 27:
+                break
+
+        # draw connectivity
+        for (i, j) in np.transpose(np.triu_indices(self._con.shape[0])):
+            if not self._con[i,j]: continue
+            p0 = tuple(self.xy2uv(self._joints[i]))
+            p1 = tuple(self.xy2uv(self._joints[j]))
+            cv2.line(self._map, p0, p1, color=(1.0,1.0,1.0), thickness=1)
+            #cv2.line(self._map, p0, p1, color=np.random.uniform(size=3), thickness=2)
+            cv2.imshow('segments', self._map)
+            k = cv2.waitKey(10)
+            if k == 27:
+                break
+
+        ## draw segments (orig)
+        #while self._seg_i < self._seg_n:
+        #    #self._map.fill(0.)
+        #    p0, p1 = self._seg[self._seg_i] # == (2,2)
+        #    self._seg_i += 1
+        #    #if np.allclose(p0, p1):
+        #    #    continue
+        #    p0 = tuple(self.xy2uv(p0))
+        #    p1 = tuple(self.xy2uv(p1))
+        #    cv2.line(self._map, p0, p1, color=np.random.uniform(size=3), thickness=1)
+
+        #    cv2.imshow('segments', self._map)
+        #    cv2.waitKey(10)
+        #    #k = cv2.waitKey(0)
+        #    #if k == 27:
+        #    #    break
+
 
         while True:
             k = cv2.waitKey(0)
